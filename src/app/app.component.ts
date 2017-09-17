8cf09b079110c28579dc53db70e650994377bf68
Dave <dcaslin@gmail.com>
2017-09-17 09:45:55 -0400

    Home button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,6 +35,7 @@ export class AppComponent implements OnInit, OnDestroy {
   year = new Date().getFullYear();
   logo = require('../assets/logo.png');
   navigation = [
+    { link: 'home', label: 'Home' },
     { link: 'search', label: 'Search Users' },
     { link: 'about', label: 'About' }
   ];
