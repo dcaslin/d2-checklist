153f42e7eaf283baf6509945f4139a5c5278b25e
Dave Caslin <dcaslin@gmail.com>
2017-11-18 09:34:38 -0500

    shows seconds in durations

---
+++ src/app/shared/pipe/pipe.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { TimingPipe } from './timing.pipe';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [TimingPipe],
+  exports: [TimingPipe]
+})
+export class PipeModule { }
---
+++ src/app/shared/pipe/timing.pipe.ts
@@ -0,0 +1,24 @@
+
+import {Pipe, PipeTransform} from '@angular/core';
+import * as moment from 'moment';
+
+@Pipe({ name: 'amnsTiming' })
+export class TimingPipe implements PipeTransform {
+
+  private static pad(num:number, size:number): string {
+      let s = num+"";
+      while (s.length < size) s = "0" + s;
+      return s;
+  }
+
+  transform(value: any, ...args: string[]): string {
+    if (typeof args === 'undefined' || args.length !== 1) {
+      throw new Error('DurationPipe: missing required time unit argument');
+    }
+    let m = moment.duration(value, args[0] as moment.unitOfTime.DurationConstructor);
+    return TimingPipe.pad(m.hours(), 2)+":"+TimingPipe.pad(m.minutes(), 2)+":"+TimingPipe.pad(m.seconds(), 2);
+
+
+    //return "xxx"+moment.duration(value, args[0] as moment.unitOfTime.DurationConstructor).humanize();
+  }
+}
