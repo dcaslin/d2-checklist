347caf9a161b564dccb8194f3fffad0ffe15bb4c
Dave <dcaslin@gmail.com>
2018-07-10 17:57:22 -0400

    Add out triumphs

--- src/app/player/player.module.ts
+++ src/app/player/player.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { EffectsModule } from '@ngrx/effects';
 import { SharedModule } from '../shared';
 import { PlayerComponent } from './player/player.component';
 import {GearComponent} from './player/gear/gear.component';
