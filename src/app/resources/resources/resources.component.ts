a7bdb27b6cfdc04c98a2eacfee81daeb72593356
Dave Caslin <dcaslin@gmail.com>
2018-09-07 00:05:08 -0400

    More work on resources page

--- src/app/resources/resources/resources.component.ts
+++ src/app/resources/resources/resources.component.ts
@@ -8,6 +8,7 @@ import { ANIMATE_ON_ROUTE_ENTER } from '../../animations/router.transition';
 import { BungieService } from "../../service/bungie.service";
 import { ChildComponent } from '../../shared/child.component';
 import { StorageService } from '../../service/storage.service';
+import { SelectedUser, Player, Character } from '@app/service/model';
 
 @Component({
   selector: 'anms-resources',
@@ -17,24 +18,54 @@ import { StorageService } from '../../service/storage.service';
 export class ResourcesComponent extends ChildComponent implements OnInit, OnDestroy {
   animateOnRouteEnter = ANIMATE_ON_ROUTE_ENTER;
 
+  selectedUser: SelectedUser = null;
+  player: Player = null;
+  char: Character = null;
+  vendorData: any = null;
+
   constructor(storageService: StorageService, private bungieService: BungieService, 
     private route: ActivatedRoute, private router: Router) {
     super(storageService);
   }
+
+  public async setChar(c: Character, alreadyLoading: boolean){
+    if (c==null){
+      this.char = null;
+      this.vendorData = null;
+      return;
+    } 
+    if (!alreadyLoading) this.loading = true;
+    try{
+      this.char = c;
+      this.vendorData = await this.bungieService.loadVendors(c);
+    }
+    finally {
+      if (!alreadyLoading) this.loading = false;
+    }
+  }
   
-  private load() {
+  private async load() {
+    if (this.selectedUser==null){
+      this.player = null;
+      this.setChar(null, false);
+      return;
+    }
     this.loading = true;
-    this.bungieService.getVendorData().then((res) => {
-      
+    try{
+      this.player = await this.bungieService.getChars(this.selectedUser.selectedUser.membershipType, this.selectedUser.selectedUser.membershipId, ['Profiles', 'Characters']);
+      await this.setChar(this.player.characters[0], true);
+    }
+    finally{
       this.loading = false;
-    }).catch((x) => {
-      this.loading = false;
-    });
+    }
   }
 
   private sub: any;
   ngOnInit() {
-    this.load();
+    this.bungieService.selectedUserFeed.pipe(takeUntil(this.unsubscribe$)).subscribe((selectedUser: SelectedUser) => {
+      this.selectedUser = selectedUser;
+      this.load();
+    });
     
   }
 }
