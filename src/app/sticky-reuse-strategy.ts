eb595ccddecc0102c33c235d984c31579f058bb8
Dave Caslin <dcaslin@gmail.com>
2018-09-18 12:01:20 -0400

    Improve router caching, display personal clan XP in progressions

--- src/app/sticky-reuse-strategy.ts
+++ src/app/sticky-reuse-strategy.ts
@@ -9,28 +9,53 @@ export class StickyReuseStrategy implements RouteReuseStrategy {
 
   handlers: {[key: string]: DetachedRouteHandle} = {};
 
-  shouldDetach(route: ActivatedRouteSnapshot): boolean {
-    if (route.routeConfig.path.indexOf("leaderboard")==0){
-      return false;
+  getPath(route: ActivatedRouteSnapshot){
+    if (route==null) return "null";
+    if (route.routeConfig==null) return "null routeConfig";
+    if (route.routeConfig.path==null) return "null routeConfig.path";
+    return route.routeConfig.path;
+  }
+
+  getPrefix(route: ActivatedRouteSnapshot){
+    if (route==null) return null;
+    if (route.routeConfig==null) return null;
+    if (route.routeConfig.path==null) return null;
+    const p = route.routeConfig.path;
+    const i = p.indexOf("/");
+    if (i>0){
+      return p.substring(0, i);
     }
+    else{
+      return p;
+    }
+  }
+
+  shouldDetach(route: ActivatedRouteSnapshot): boolean {
     return true;
   }
 
   store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
-    this.handlers[route.routeConfig.path] = handle;
+    this.handlers[this.getPrefix(route)] = handle;
   }
 
   shouldAttach(route: ActivatedRouteSnapshot): boolean {
-    return !!route.routeConfig && !!this.handlers[route.routeConfig.path];
+    const prefix = this.getPrefix(route);
+    return !!prefix && !!this.handlers[prefix];
   }
 
   retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
-    if (!route.routeConfig) return null;
-    return this.handlers[route.routeConfig.path];
+    let returnMe = null;
+    const prefix = this.getPrefix(route);
+    if (prefix!=null && this.handlers[prefix]!=null) {
+      returnMe = this.handlers[prefix];
+    }
+    // console.log("*    retrieving route: "+this.getPath(route)+": "+(returnMe!=null));
+    return returnMe;
   }
 
   shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
-    return future.routeConfig === curr.routeConfig;
+    const should = this.getPrefix(future) === this.getPrefix(curr);
+    return should;
   }
 
 }
