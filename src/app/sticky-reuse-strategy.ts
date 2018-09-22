8f042bd0e16854dfc7c0c7aa943d515fcaa3d3c3
Dave Caslin <dcaslin@gmail.com>
2018-09-22 08:02:15 -0400

    Saving broken reuse strategy to remember it

--- src/app/sticky-reuse-strategy.ts
+++ src/app/sticky-reuse-strategy.ts
@@ -22,6 +22,7 @@ export class StickyReuseStrategy implements RouteReuseStrategy {
     if (route.routeConfig.path==null) return null;
     const p = route.routeConfig.path;
     const i = p.indexOf("/");
+    //const i = p.lastIndexOf("/");
     if (i>0){
       return p.substring(0, i);
     }
@@ -30,8 +31,16 @@ export class StickyReuseStrategy implements RouteReuseStrategy {
     }
   }
 
+  //Variable child routes are horrible for caching, we only use those for vendors and leaderboards
+  //There's little benefit to caching there anyway
+  dontSave(route: ActivatedRouteSnapshot):boolean{
+    const prefix = this.getPrefix(route);
+    if (prefix=="vendors" || prefix=="leadboard") return true;
+    return false;
+  }
+
   shouldDetach(route: ActivatedRouteSnapshot): boolean {
-    return true;
+    return !this.dontSave(route);
   }
 
   store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
@@ -39,6 +48,7 @@ export class StickyReuseStrategy implements RouteReuseStrategy {
   }
 
   shouldAttach(route: ActivatedRouteSnapshot): boolean {
+    if (this.dontSave(route)) return false;
     const prefix = this.getPrefix(route);
     return !!prefix && !!this.handlers[prefix];
   }
@@ -49,12 +59,15 @@ export class StickyReuseStrategy implements RouteReuseStrategy {
     if (prefix!=null && this.handlers[prefix]!=null) {
       returnMe = this.handlers[prefix];
     }
-    // console.log("*    retrieving route: "+this.getPath(route)+": "+(returnMe!=null));
+    console.log("*    retrieving route: "+route.routeConfig.path+": "+(returnMe!=null));
     return returnMe;
   }
 
   shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
     const should = this.getPrefix(future) === this.getPrefix(curr);
+    if (should){
+      console.log("Reuse: "+this.getPath(curr)+"->"+this.getPath(future));
+    }
     return should;
   }
 
