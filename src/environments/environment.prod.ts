b6dc5797cd5de0c724e679814fdec626ea2100e8
Dave Caslin <dcaslin@gmail.com>
2019-02-25 22:37:32 -0500

    Update API keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,8 @@
-const packageJson = require('../../package.json');
+import { bungieProd } from "./keys";
 
+const packageJson = require('../../package.json');
 export const environment = {
-  bungie: {
-    apiKey: 'a313d9ebfcd741688d76ab8e9549f322',
-    authUrl: 'https://www.bungie.net/en/OAuth/Authorize',
-    clientId: '21084',
-    clientSecret: 'VfL62EnXGVQK3mYJadHW4Dhi-2i7jamZGm7oEPjDWVo'
-  },
-  xyzApiKey: '2a35cd994912b3b167741c621f381234',
+  bungie: bungieProd,
   production: true,
   versions: {
     app: packageJson.version,
