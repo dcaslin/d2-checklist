fa3b972059901d8d069496673cab4bd45115c9a2
Dave <dcaslin@gmail.com>
2019-03-04 09:44:43 -0500

    IE11 support

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -54,3 +54,6 @@ import 'web-animations-js';  // Run `npm install --save web-animations-js`.
  * Zone JS is required by Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
+
+/** IE11 Array to support .includes(), CUSTOM ADDED */
+import 'core-js/es7/array';
