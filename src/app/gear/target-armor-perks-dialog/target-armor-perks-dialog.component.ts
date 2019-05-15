c712deb8dc8d43c952798b91795d53462b90ed82
Dave Caslin <dcaslin@gmail.com>
2019-05-14 21:35:23 -0400

    More fully featured armor target perks

--- src/app/gear/target-armor-perks-dialog/target-armor-perks-dialog.component.ts
+++ src/app/gear/target-armor-perks-dialog/target-armor-perks-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { GearComponent } from '../gear/gear.component';
 import { TargetPerkService, TargetPerks } from '@app/service/target-perk.service';
+import { Choice } from '../gear/gear-toggle.component';
 
 @Component({
   selector: 'anms-target-armor-perks-dialog',
@@ -11,12 +12,25 @@ import { TargetPerkService, TargetPerks } from '@app/service/target-perk.service
 export class TargetArmorPerksDialogComponent implements OnInit {
   parent: GearComponent;
   target: TargetPerks;
+  targetChoices: Choice[];
 
   constructor(
     private targetPerkService: TargetPerkService,
     public dialogRef: MatDialogRef<TargetArmorPerksDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) {
     this.parent = data.parent;
+    const choices = this.parent.weaponTypeChoices.slice(0);
+    choices.push(new Choice('Super', 'Super', true));
+    choices.push(new Choice('Melee', 'Melee', true));
+    choices.push(new Choice('Grenade', 'Grenade', true));
+    choices.push(new Choice('Ability', 'Ability', true));
+    const d = {};
+    for (const c of choices){
+      d[c.matchValue.toLowerCase()] = '';
+    }
+    console.dir(d);
+
+    this.targetChoices = choices;
     this.target = this.targetPerkService.perks.value;
     dialogRef.afterClosed().subscribe(result => {
       this.targetPerkService.update(this.target);
