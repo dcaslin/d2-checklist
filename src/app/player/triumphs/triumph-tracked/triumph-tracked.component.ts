bd562814e2c41e1c8c7b2c5de5499159d6fcc349
Dave Caslin <dcaslin@gmail.com>
2019-06-12 18:51:50 -0400

    Triumphs done!

--- src/app/player/triumphs/triumph-tracked/triumph-tracked.component.ts
+++ src/app/player/triumphs/triumph-tracked/triumph-tracked.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectorRef, Output, EventEmitter, ViewChild, ChangeDetectionStrategy } from '@angular/core';
+import { takeUntil, debounceTime } from 'rxjs/operators';
+import { ChildComponent } from '@app/shared/child.component';
+import { StorageService } from '@app/service/storage.service';
+import { TriumphNode, TriumphRecordNode, Player } from '@app/service/model';
+import { Subject, of as observableOf, BehaviorSubject, Observable } from 'rxjs';
+import { MatTabGroup, MatTreeFlattener, MatTreeFlatDataSource } from '@angular/material';
+import { FlatTreeControl } from '@angular/cdk/tree';
+import { PlayerStateService } from '../../player-state.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'd2c-triumph-tracked',
   templateUrl: './triumph-tracked.component.html',
-  styleUrls: ['./triumph-tracked.component.scss']
+  styleUrls: ['./triumph-tracked.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TriumphTrackedComponent implements OnInit {
+export class TriumphTrackedComponent extends ChildComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(storageService: StorageService,
+    public state: PlayerStateService,
+    private route: ActivatedRoute, 
+    private router: Router,
+    private ref: ChangeDetectorRef) {
+    super(storageService, ref);
+  }
   ngOnInit() {
   }
 
