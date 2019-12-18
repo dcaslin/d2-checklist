7a63b448c5aff956ec580d647882c5a96811797d
Dave <dcaslin@gmail.com>
2019-12-18 18:31:08 -0500

    downgrade FontAwesome which was breaking JUST prod

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,71 +1,32 @@
-import { NgModule } from '@angular/core';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { CdkTableModule } from '@angular/cdk/table/';
 import { CommonModule } from '@angular/common';
-import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
-
-import { PipeModule} from './pipe';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {
-  MatButtonModule,
-  MatButtonToggleModule,
-  MatToolbarModule,
-  MatMenuModule,
-  MatTreeModule,
-  MatSelectModule,
-  MatTabsModule,
-  MatInputModule,
-  MatProgressSpinnerModule,
-  MatChipsModule,
-  MatSidenavModule,
-  MatCheckboxModule,
-  MatAutocompleteModule,
-  MatCardModule,
-  MatDialogModule,
-  MatListModule,
-  MatIconModule,
-  MatTooltipModule,
-  MatSnackBarModule,
-  MatProgressBarModule,
-  MatExpansionModule,
-
-  MatSortModule,
-  MatPaginatorModule,
-  MatTableModule
-} from '@angular/material';
-
-
+import { MatAutocompleteModule, MatButtonModule, MatButtonToggleModule, MatCardModule, MatCheckboxModule, MatChipsModule, MatDialogModule, MatExpansionModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule, MatSelectModule, MatSidenavModule, MatSnackBarModule, MatSortModule, MatTableModule, MatTabsModule, MatToolbarModule, MatTooltipModule, MatTreeModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AdsenseModule } from 'ng2-adsense';
 import { MomentModule } from 'ngx-moment';
-
-import {
-  CdkTableModule
-} from '@angular/cdk/table/';
-import { ScrollingModule } from '@angular/cdk/scrolling';
-import { RouterModule } from '@angular/router';
-import { MilestoneCheckComponent } from './milestone-check/milestone-check.component';
-
-
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
-import { SignInRequiredComponent } from './sign-in-required/sign-in-required.component';
-import { SortIndicatorComponent } from './sort-indicator/sort-indicator.component';
-import { SeasonIndicatorComponent } from './season-indicator/season-indicator.component';
 import { ItemEnergyIndicatorComponent } from './item-energy-indicator/item-energy-indicator.component';
 import { ItemIconComponent } from './item-icon/item-icon.component';
-
+import { MilestoneCheckComponent } from './milestone-check/milestone-check.component';
+import { PipeModule } from './pipe';
+import { SeasonIndicatorComponent } from './season-indicator/season-indicator.component';
+import { SignInRequiredComponent } from './sign-in-required/sign-in-required.component';
+import { SortIndicatorComponent } from './sort-indicator/sort-indicator.component';
 
 @NgModule({
   imports: [
     PipeModule,
     CommonModule,
     FormsModule,
-    
     FontAwesomeModule,
-
     MomentModule,
     AdsenseModule.forRoot({
       adClient: 'ca-pub-4577479845324857',
       adSlot: 7862857321 // 7862857321 responsive right 6246523328 responsive banner
     }),
-
     MatButtonModule,
     MatButtonModule,
     MatToolbarModule,
@@ -110,15 +71,12 @@ import { ItemIconComponent } from './item-icon/item-icon.component';
     SignInRequiredComponent,
     RouterModule,
     PipeModule,
-
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
     MomentModule,
     AdsenseModule,
-
     FontAwesomeModule,
-    
     MatButtonModule,
     MatButtonToggleModule,
     MatMenuModule,
@@ -146,8 +104,7 @@ import { ItemIconComponent } from './item-icon/item-icon.component';
     ScrollingModule
   ]
 })
-export class SharedModule { 
-  constructor(library: FaIconLibrary) {
-    library.addIcons(faCoffee);
+export class SharedModule {
+  constructor() {
   }
 }
