bba75dc33fbd4cfa8befb85006c5bfa4d5efd5b5
dcaslin <dcaslin@gmail.com>
2019-12-25 10:00:12 -0500

    bounty work

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,15 +5,17 @@ import { SharedModule } from '../shared';
 
 import { HomeComponent } from './home/home.component';
 import { BurnDialogComponent } from './home/burn-dialog/burn-dialog.component';
+import { BountySetsDialogComponent } from './home/bounty-sets-dialog/bounty-sets-dialog.component';
 
 @NgModule({
   imports: [
     SharedModule
   ],
-  declarations: [HomeComponent, BurnDialogComponent,
+  declarations: [HomeComponent, BurnDialogComponent, BountySetsDialogComponent,
   ], 
   entryComponents: [
-    BurnDialogComponent
+    BurnDialogComponent, 
+    BountySetsDialogComponent
   ]
 })
 export class HomeModule { }
