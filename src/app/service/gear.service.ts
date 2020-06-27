eb466d7627ed2675f9d3faf159fb29cb6971c4b1
dcaslin <dcaslin@gmail.com>
2020-06-27 17:35:11 -0400

    mid-work

--- src/app/service/gear.service.ts
+++ src/app/service/gear.service.ts
@@ -136,6 +136,7 @@ export class GearService {
             }
             this.pandaService.processItems(player.gear);
             this.preferredStatService.processGear(player);
+            this.bucketService.markHighest();
             return player;
         } finally {
             this.loading.next(false);
