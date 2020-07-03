ace4c73f51b8f291f22c3a131869d144b8206179
dcaslin <dcaslin@gmail.com>
2020-07-03 14:34:22 -0400

    Fix API key

--- src/app/todo-list/services/http.service.ts
+++ src/app/todo-list/services/http.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AuthInfo, AuthService } from '@app/service/auth.service';
-import { bungieDev } from '@env/keys';
+import { environment } from '@env/environment';
 import { Observable } from 'rxjs';
 
 /**
@@ -20,7 +20,7 @@ export class HttpService {
   public get(url: string, options?: any): Observable<any> {
     options = options || {};
     let headers = new HttpHeaders(options.headers)
-      .set('x-api-key', bungieDev.apiKey);
+      .set('x-api-key', environment.bungie.apiKey);
 
     // add the auth header if we have an auth key
     headers = this.authHeader ? headers.set('authorization', this.authHeader) : headers;
