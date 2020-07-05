a9b49fb61500f5c7c800771444ec3262961aa765
dcaslin <dcaslin@gmail.com>
2020-07-05 17:12:13 -0400

    Fix build for manifest

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   production: false,
   versions: {
     app: packageJson.version,
-    manifest: '84762.20.06.16.1703-1',
+    manifest: packageJson.manifest,
     angular: packageJson.dependencies['@angular/core'],
     material: packageJson.dependencies['@angular/material'],
     bootstrap: packageJson.dependencies.bootstrap,
