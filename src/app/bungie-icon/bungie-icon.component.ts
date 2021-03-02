722c8464ad27014e3575231d70d40d9c31c1335b
Dave <dcaslin@gmail.com>
2021-03-02 11:03:08 -0500

    Icon updates

--- src/app/bungie-icon/bungie-icon.component.ts
+++ src/app/bungie-icon/bungie-icon.component.ts
@@ -8,10 +8,10 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
   `
 })
 export class BungieIconComponent {
-
-  @Input() set icon(path: string) { this.iconUrl = `//www.bungie.net${path}`; }
-  @Input() iconClass: string = 'icon';
+  @Input() iconClass = 'icon';
 
   public iconUrl: string;
+
+  @Input() set icon(path: string) { this.iconUrl = `//www.bungie.net${path}`; }
 
 }
