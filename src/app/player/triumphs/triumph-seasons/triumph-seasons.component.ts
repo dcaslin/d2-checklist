82890047fa25a21e0db1ccba5fc373835da78a0b
Dave <dcaslin@gmail.com>
2021-05-11 15:17:25 -0400

    Season of splicer

--- src/app/player/triumphs/triumph-seasons/triumph-seasons.component.ts
+++ src/app/player/triumphs/triumph-seasons/triumph-seasons.component.ts
@@ -23,7 +23,7 @@ export class TriumphSeasonsComponent extends ChildComponent implements OnInit {
     super(storageService);
     const maxEntries = this.state.currPlayer()?.seasonChallengeEntries.length;
     if (maxEntries) {
-      const week = WeekService.getChosenWeek();
+      const week = WeekService.getSplicerWeek();
       if (week < maxEntries) {
         this.seasonIndex = week;
       }
--- src/app/service/model.ts
+++ src/app/service/model.ts
@@ -1278,10 +1278,10 @@ export class Const {
     // public static readonly PSUEDO_PRESAGE = 'PSUEDO_PRESAGE';
     public static readonly PSUEDO_MASTER_EMPIRE_HUNT = 'PSUEDO_MASTER_EMPIRE';
 
-    public static readonly LIGHT_TOO_LOW = 1249; // update me
-    private static readonly SEASON_SOFT_CAP = 1250; // update me
-    public static readonly SEASON_HARD_CAP = 1300; // update me
-    public static readonly SEASON_PINNACLE_CAP = 1310; // update me
+    public static readonly LIGHT_TOO_LOW = 1259; // update me
+    private static readonly SEASON_SOFT_CAP = 1260; // update me
+    public static readonly SEASON_HARD_CAP = 1310; // update me
+    public static readonly SEASON_PINNACLE_CAP = 1320; // update me
 
 
     public static readonly BOOST_UNKNOWN = 'BOOST_UNKNOWN';
