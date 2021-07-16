5bfa23456ed3a7aaf1cbd1775b024e788291b0e1
Dave Caslin <dcaslin@gmail.com>
2021-07-16 19:03:51 -0400

    more work

--- src/app/pgcr/pgcr.module.ts
+++ src/app/pgcr/pgcr.module.ts
@@ -5,11 +5,12 @@ import { SharedModule } from '../shared';
 
 import { PGCRComponent } from './pgcr/pgcr.component';
 import { Pgcr2Component } from './pgcr2/pgcr2.component';
+import { PgcrEntryDialogComponent } from './pgcr-entry-dialog/pgcr-entry-dialog.component';
 
 @NgModule({
   imports: [
     SharedModule
   ],
-  declarations: [PGCRComponent, Pgcr2Component]
+  declarations: [PGCRComponent, Pgcr2Component, PgcrEntryDialogComponent]
 })
 export class PGCRModule { }
